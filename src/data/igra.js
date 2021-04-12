6556680fbeee3626ebd95c8b14a01040a67e6d14
ervin <ervin.schaff@academia.si>
2021-04-12 18:21:25 +0200

    :new: prazna igra

---
+++ src/data/igra.js
@@ -0,0 +1,20 @@
+
+import mojaOseba from '@/data/oseba.json'
+
+let prva = 'asas';
+
+export let delaj = function (msg) {
+    return mojaOseba.name + 'IZPISI' + msg;
+};
+
+export let delaj2 = function (msg) {
+    return mojaOseba.name + 'IZPISI' + msg;
+};
+
+
+export default prva
+
+
+console.log(prva)
+
+console.log(delaj2)
