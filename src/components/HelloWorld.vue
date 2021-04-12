7129bdbc245d0e0499ab233b52a1ecd3832b4665
Rok989 <rok.krajnc@academiavss.si>
2021-04-12 21:25:36 +0200

    deleted empty line

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,6 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
